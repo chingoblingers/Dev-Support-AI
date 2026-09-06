ef317b320d54e19c9e4c1abb46d449ded1d54dc3
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-09-06 00:52:39 -0500

    installed sentecte-transfomers pkg and imported class to build embed_question function.

--- python-server/main.py
+++ python-server/main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from sentence_transformers import SentenceTransformer
+
 
 app = FastAPI()
+model = SentenceTransformer("all-MiniLM-L6-v2") 
 
 class QuestionValidator(BaseModel):
-    question: str
+    question: str  
 
 @app.post('/search')
 def questionSimilarities(question: QuestionValidator):
-    return question
+    embeddings = embed_question(question.question)
+    
+
+def embed_question(question:str)-> list[float]:
+    embedding = model.encode(question)
+    return embedding.tolist()
