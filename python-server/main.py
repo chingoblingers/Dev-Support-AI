f3636504907c044cb19531e79a465c4c10c3e377
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-09-06 04:31:51 -0500

    created knowledge_chunks list and used it to create stored chunks dict

--- python-server/main.py
+++ python-server/main.py
@@ -17,3 +17,11 @@ def questionSimilarities(question: QuestionValidator):
 def embed_question(question:str)-> list[float]:
     embedding = model.encode(question)
     return embedding.tolist()
+
+knowledge_chunks = [
+    "Openai with Vercel AI Sdk is the default AI model and documentation used in Kenton's Projects",
+    "If Typescript is showing an error for the return code. Try Hovering over function to see the returned result for more information",
+    "If there is a line between a piece of code. Hover over to view VScode's notes on the matter. A function or method could be depreciated and needs to be updated."
+                    ]
+
+stored_chunks = [{'content': chunk , 'embedding': embed_question(chunk)} for chunk in knowledge_chunks]
