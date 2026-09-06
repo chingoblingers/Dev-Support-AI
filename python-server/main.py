478929191899882ab27c01f93891897b90b90d20
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-09-06 05:38:16 -0500

    stored encoded the knowledge chunks and stored the embeddings in stored_vectors

--- python-server/main.py
+++ python-server/main.py
@@ -4,6 +4,13 @@ from sentence_transformers import SentenceTransformer, util
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
+knowledge_chunks = [
+    "Openai with Vercel AI Sdk is the default AI model and documentation used in Kenton's Projects",
+    "If Typescript is showing an error for the return code. Try Hovering over function to see the returned result for more information",
+    "If there is a line between a piece of code. Hover over to view VScode's notes on the matter. A function or method could be depreciated and needs to be updated."
+]
+
+stored_vectors = model.encode(knowledge_chunks, convert_to_tensor=True)
 
 """
 app = FastAPI()
@@ -20,11 +27,6 @@ def embed_question(question:str)-> list[float]:
     embeddings = model.encode(question)
     return embeddings.tolist()
 
-knowledge_chunks = [
-    "Openai with Vercel AI Sdk is the default AI model and documentation used in Kenton's Projects",
-    "If Typescript is showing an error for the return code. Try Hovering over function to see the returned result for more information",
-    "If there is a line between a piece of code. Hover over to view VScode's notes on the matter. A function or method could be depreciated and needs to be updated."
-                    ]
 
 stored_chunks = [{'content': chunk , 'embedding': embed_question(chunk)} for chunk in knowledge_chunks]
 
