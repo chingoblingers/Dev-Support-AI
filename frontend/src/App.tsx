bdbb82a424b8b416df0062f70f216ce35b26d21b
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-09-08 02:29:11 -0500

    created type and set up intial state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 
+type ChatMessage = {"role": 'user' | 'assistant', 'message': string, 'sources'?: string[]}
+
+const [userInput, setUserInput] = useState<string>('')
+const [loading, setLoading] = useState<boolean>(false)
+const [chatHistory, setChatHistory] = useState<ChatMessage[]>([])
+const [error, setError] = useState<string>("")
+
 function App() {
 
   return ('Hello')
